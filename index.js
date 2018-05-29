6907f55dec452ac5e0cd9ad175de88af11c3593c
SVandehey <smvandehey@gmail.com>
2018-05-29 16:39:09 -0400

    added the browse page and a link to it in the image

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ router.get('/',function(req, res){
   res.sendFile(path + 'index.html');
 });
 
+router.get('/browse',function(req, res){
+  res.sendFile(path + 'browse.html');
+});
+
 app.use(express.static(__dirname + '/public/stylesheets'));
 app.use(express.static(__dirname + '/public/images'));
 
 app.use('*',function(req, res){
   res.send('Error 404: Not Found!');
 });
-  
 app.listen(3000,function(){
   console.log("Server running at Port 3000");
-});
+});  
