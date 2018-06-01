b12f2b9d7e8dc76648ba0b6cef0e91ce8ae893db
SVandehey <smvandehey@gmail.com>
2018-06-01 15:46:56 -0400

    Created a Database model and some dummy data for the recipe table
    This should be enough to start dynamically loading the browse page

---
+++ recipieDummyData.sql
@@ -0,0 +1,11 @@
+use mydb;
+
+INSERT INTO Recipe (name, description) VALUES ('Greenbean Brocolli Soup', 'Hearty soup with rich creamy textures Perfect for a rainy day.');
+INSERT INTO Recipe (name, description) VALUES ('Breaded Zuccini', 'A simple Healthy snack, fan favorite among kids');
+INSERT INTO Recipe (name, description) VALUES ('Tomato Basil Pasta', 'A few easy additions to spice up store bought red sauce');
+INSERT INTO Recipe (name, description) VALUES ('Fried Tofu', 'Cooked in a spicy thai sauce, a perfect addition to salads and stir-fries');
+INSERT INTO Recipe (name, description) VALUES ('Simple Radish Salad','Iceberg lettuce base with dried cranberries and homeade croutons');
+INSERT INTO Recipe (name, description) VALUES ('Weeknight Tostada with blackeyed peas', 'Easy Taco Tuesday meal topped with sala and mexican rice');
+INSERT INTO Recipe (name, description) VALUES ('Homemade Trail-mix', 'Simple starter recipe that goes well with any stir-ins');
+INSERT INTO Recipe (name, description) VALUES ('Asian Stir Fry with Creamy Mushroom Sauce', 'Great recipe to use the last of your summer vegtables including zuccuni and hot peppers');
+INSERT INTO Recipe (name, description) VALUES ('Green Broccoli soup','A Hearty dish with rich creamy textures well suited for a rainy day.');
