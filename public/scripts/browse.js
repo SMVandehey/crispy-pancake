77e5f9224e8268d32dab0dbee3f5022a54a25ec2
SVandehey <smvandehey@gmail.com>
2018-06-03 23:14:17 -0400

    Added dynamic image loading
    renamed the image files to follow the id scheme set up in the db

--- public/scripts/browse.js
+++ public/scripts/browse.js
@@ -15,7 +15,10 @@ function bindRecipies(query)
 		var description = query[i].description;
 
 		var id = "#card" + i;
+		var imagePath = '/images/food' + i + '.jpg';
 		var body = $(id).children(".card-body");
+
+		$(id).children(".card-img-top").attr('src', imagePath);
 		$(body).children(".card-title").text(name);
 		$(body).children(".description").text(description);
 	}
