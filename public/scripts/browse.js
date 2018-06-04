ef843f48353b460690729f010bb6e2bd8bfbc06d
SVandehey <smvandehey@gmail.com>
2018-06-03 22:19:41 -0400

    Browse page is now dynamically loaded
    Now I need real images for the recipues but that's okay!

--- public/scripts/browse.js
+++ public/scripts/browse.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
 
    $.get("/getBrowseInfo", function(data, status){
-   		obj = JSON.parse(data);
-   		console.log(obj);
+   		var obj = JSON.parse(data);
+   		bindRecipies(obj);
    }); 
 
 }); 
+
+function bindRecipies(query)
+{
+	for(i = 0; i < 9; i++)
+	{
+		var name = query[i].name;
+		var description = query[i].description;
+
+		var id = "#card" + i;
+		var body = $(id).children(".card-body");
+		$(body).children(".card-title").text(name);
+		$(body).children(".description").text(description);
+	}
+}
