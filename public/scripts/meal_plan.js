22fb068822e9ebd3c44617a925c31fffd9de2c49
SVandehey <smvandehey@gmail.com>
2018-06-11 17:46:22 -0400

    now dynamically loads meal plan

--- public/scripts/meal_plan.js
+++ public/scripts/meal_plan.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
     $.get('/generate', function(data, status){
-        
+        bindRecipes(data);
     });
 });
+
+function bindRecipes(data)
+{
+    var result = JSON.parse(data);
+
+    for(i = 0; i < 5; i++)
+    {
+        var name = result[i].name;
+        var description = result[i].description
+        var id = result[i].recipeID;
+
+        var cardID = "#card" + i;
+        var imagePath = '/images/food' + (id - 1) + '.jpg';
+        var body = $(cardID).children(".card-body");
+
+        $(cardID).children(".card-img-top").attr('src', imagePath);
+        $(body).children(".card-title").text(name);
+        $(body).children(".description").text(description);
+    }
+}
